93a88de8c44908e81e17723f68e9cea367cf9cd2
HamliriSanaa <144817752+HamliriSanaa@users.noreply.github.com>
2024-01-08 20:58:00 +0100

    login front end

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include_once '../wiki_tm/includes/header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../wiki_tm/assets/css/style.css">
+</head>
+<body>
+    <h1>Hi! Page Home</h1>
+</body>
+</html>
