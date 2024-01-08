f66c6be6bc1637ab951bfd1f7440dce9a65d708a
HamliriSanaa <144817752+HamliriSanaa@users.noreply.github.com>
2024-01-08 20:36:18 +0100

    sign up form front end

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<?php 
+    session_start(); 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Login System</title>
+    <link rel="stylesheet" href="../assets/css/style.css" type="text/css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <a href="index.php"><li>Home</li></a>
+                <a href="../views/signup.php"><li>Sign Up</li></a>
+                <a href="../views/login.php"><li>Login</li></a>
+                <a href="./controllers/Users.php?q=logout"><li>Logout</li></a>
+        </ul>
+    </nav>
